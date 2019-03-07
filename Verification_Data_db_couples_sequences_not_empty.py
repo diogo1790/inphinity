ba400a77c9e940aff69f5b19f43e92d322faf771
diogom1790 <diogo.leite@heig-vd.ch>
2019-03-07 16:38:06 +0100

    add methos to perform each organism #25

--- Verification_Data_db_couples_sequences_not_empty.py
+++ Verification_Data_db_couples_sequences_not_empty.py
@@ -1,6 +1,8 @@
 from configuration.configuration_api import ConfigurationAPI
 from rest_client.AuthenticationRest import AuthenticationAPI
 
+import json
+import numpy as np
 
 
 from objects_API.CoupleJ import CoupleJson
@@ -8,6 +10,23 @@ from objects_API.ProteinJ import ProteinJson
 from objects_API.WholeDNAJ import WholeDNAJson
 from objects_API.ContigJ import ContigJson
 
+
+def writeDictProteinError(dict_protein_error:dict, organism_id:int, path_base:str):
+    path_base = path_base + 'proteins_'
+    writeDictsErrors(dict_protein_error, path_base, organism_id)
+
+def writeDictWholeDNAError(dict_protein_error:dict, organism_id:int, path_base:str):
+    path_base = path_base + 'WD_'
+    writeDictsErrors(dict_protein_error, path_base, organism_id)
+
+def writeDictContigError(dict_protein_error:dict, organism_id:int, path_base:str):
+    path_base = path_base + 'Contig_'
+    writeDictsErrors(dict_protein_error, path_base, organism_id)
+
+def writeDictsErrors(dictionaty_error:dict, path_file_write:str, organism_id:int):
+    path_write_json = path_file_write + str(organism_id) + '.json'
+    with open(path_write_json, 'w') as file:
+         file.write(json.dumps(dictionaty_error)) # use `json.loads` to do the reverse
 
 
 def checkSequenceProteins(list_proteins:ProteinJson):
@@ -39,11 +58,15 @@ def checkWholeDna(whole_dna):
     :rtype: dict[id_whole_dna]:sequence
     """
     dict_whole_dna_error = {}
-    if len(whole_dna.sequence_DNA) < 1000:
+
+    if whole_dna is None:
+        dict_whole_dna_error['No whole DNA'] = 'No Whole DNA'
+
+    elif len(whole_dna.sequence_DNA) < 100:
         dict_whole_dna_error[whole_dna.id] = whole_dna.sequence_DNA
     return dict_whole_dna_error
 
-def checkSequenceContigs(list_contigs):
+def checkSequenceContigs(list_contigs:ContigJson):
     """
     check if the sequence exists for a contig given a list of them
 
@@ -51,14 +74,22 @@ def checkSequenceContigs(list_contigs):
     :rtype: dict[id_contig]:sequence
     """
     dict_contigs_error = {}
-    for contig in list_contigs:
-        if len(contig.sequence_DNA) < 15:
-            print('error')
-            id_contig = contig.id
-            contig_sequence = contig.sequence_DNA
-            dict_contigs_error[id_contig] = contig_sequence
+
+    if list_contigs is None:
+        dict_contigs_error['No whole Contig'] = 'No Whole Contig'
+    else:
+        for contig in list_contigs:
+            if len(contig.sequence_DNA) < 15:
+                print('error')
+                id_contig = contig.id
+                contig_sequence = contig.sequence_DNA
+                dict_contigs_error[id_contig] = contig_sequence
 
     return dict_contigs_error
+
+
+
+
 
 def getAllCouples():
     list_couple = CoupleJson.getAllAPI()
@@ -70,14 +101,56 @@ def getProteinsListByOrganismId(organism_id:int):
     return list_proteins
 
 def getWholeGenomeByOrganismId(organism_id:int):
-    whole_dna_obj = WholeDNAJson.getByOrganismID(organism_id)
-    return whole_dna_obj
+    try:
+        whole_dna_obj = WholeDNAJson.getByOrganismID(organism_id)
+        return whole_dna_obj
+    except:
+        return None
+
 
 
 def getContigsByOrganismId(organism_id:int):
     list_contig = ContigJson.getByOrganismID(organism_id)
     return list_contig
 
+
+def organismValidation(id_organism:int, path_to_save:str):
+
+    list_proteins = getProteinsListByOrganismId(id_organism)
+    whole_dna_obj = getWholeGenomeByOrganismId(id_organism)
+    list_contigs = getContigsByOrganismId(id_organism)
+    dict_error_protein = {}
+    dict_errors_whole_dna = {}
+    dict_errors_contig = {}
+
+    dict_errors_whole_dna = checkWholeDna(whole_dna_obj)
+    dict_errors_contig = checkSequenceContigs(list_contigs)
+    dict_error_protein = checkSequenceProteins(list_proteins)
+
+    if len(dict_error_protein) > 0:
+        writeDictProteinError(dict_error_protein, bacterium_id, path_write)
+
+    if len(dict_errors_contig) > 0:
+        writeDictContigError(dict_errors_contig, bacterium_id, path_write)
+
+    if len(dict_errors_whole_dna) > 0:
+        writeDictWholeDNAError(dict_errors_whole_dna, bacterium_id, path_write)
+
+    return True
+
+def writeOrganismValidated(path_save:str, list_ids_bacteria:np):
+    np.savetxt(path_save, list_ids_bacteria, delimiter=",", fmt='%d')
+
+def readOrganismValidated(path_save:str):
+    list_ids_organigms = []
+
+    list_ids_organigms = np.genfromtxt(path_save, delimiter=',')
+
+    return list_ids_organigms
+
+path_organisme_validated = 'error_organisms/organisms_validated.csv'
+
+list_ids_bacteria_validated = readOrganismValidated(path_organisme_validated)
 
 conf_obj = ConfigurationAPI()
 conf_obj.load_data_from_ini()
@@ -93,13 +166,17 @@ list_ids_bacteria = list(set(list_ids_bacteria))
 list_ids_phage = [couple_obj.bacteriophage for couple_obj in list_couples]
 list_ids_phage = list(set(list_ids_phage))
 
-bacterium_id = list_ids_bacteria[0]
-
-list_proteins = getProteinsListByOrganismId(bacterium_id)
-whole_dna_obj = getWholeGenomeByOrganismId(bacterium_id)
-list_contigs = getContigsByOrganismId(bacterium_id)
 
 
+for bacterium_id in list_ids_bacteria:
+
+    path_write = 'error_organisms/bacterium/'
+
+    if bacterium_id not in list_ids_bacteria_validated:
+        result_insertion = organismValidation(bacterium_id, path_write)
+        if result_insertion:
+            list_ids_bacteria_validated = np.append(list_ids_bacteria_validated, bacterium_id)
+            writeOrganismValidated(path_organisme_validated, list_ids_bacteria_validated)
 
 print('Hello')
 print('Hello')
